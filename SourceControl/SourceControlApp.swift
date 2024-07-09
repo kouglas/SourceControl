7d85f84a494e466494c4ccc925a44372b44f8001
Kari <kouglas2@gmail.com>
2024-07-09 14:32:45 -0500

    Initial Commit

---
+++ SourceControl/SourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlApp.swift
+//  SourceControl
+//
+//  Created by Kari Douglas on 7/9/24.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
