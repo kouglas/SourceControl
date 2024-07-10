af506e143b480876e6966798b0da4b2eabd8345e
Kari <kouglas2@gmail.com>
2024-07-10 16:59:49 -0500

    create HomeView screen

---
+++ SourceControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SourceControl
+//
+//  Created by Kari Douglas on 7/10/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Screen 2 ")
+    }
+}
+
+#Preview {
+    HomeView()
+}
